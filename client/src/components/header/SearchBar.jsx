79c9ef3d69ccfec64a62b60d88c6d7ad8bfb0fc9
kwngmin <kwngmink@gmail.com>
2022-10-28 15:16:16 +0900

    feat:header 구조 및 css 일부 구현 그리고 footer 수정

---
+++ client/src/components/header/SearchBar.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function SearchBar() {
+  const [keywords, setKeywords] = useState("");
+
+  return (
+    <form className="w-full min-w-40 ml-10 mr-4 relative">
+      <span className="material-icons absolute left-3 h-10 leading-10 text-emerald-500 text-3xl">search</span>
+      <input
+        type="text"
+        className="w-full h-10 pr-4 pl-12 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"
+        value={keywords}
+        placeholder="Search.."
+        onChange={e => {
+          setKeywords(e.target.value);
+        }}
+      />
+    </form>
+  );
+}
