f78e9763a85ec99bcefbe6f3cf80ac8a90fba9e3
ethanJcoding <ethan.95j@gmail.com>
2022-10-28 13:47:17 +0900

    style: tag 컴포넌트 디자인 변경

--- client/src/components/question/QuestionList.jsx
+++ client/src/components/question/QuestionList.jsx
@@ -1,14 +1,13 @@
 //질문하기 버튼을 누르면 보이는 창이다.
 // /ask [POST] => { title , body , tags }
 import { useState } from "react";
+import Tag from "./Tag.jsx";
 
 const Question = () => {
   const [question, setQuestion] = useState({});
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [tagItem, setTagItem] = useState("");
-  const [tagList, setTagList] = useState([]);
-  const [tagAmount, setTagAmout] = useState(0);
+
   // const [textarea, setTextArea] = useState({
   //   value: "",
   //   rows: 10,
@@ -17,39 +16,8 @@ const Question = () => {
   // });
 
   const onPostClick = () => {
-    setQuestion({ title, body, tagList });
+    setQuestion({ title, body });
     console.log(question);
-  };
-
-  const onKeyPress = (e) => {
-    if (
-      e.target.value !== "" &&
-      e.key === "Enter" &&
-      tagList.indexOf(e.target.value) === -1
-    ) {
-      submitTagItem();
-    }
-  };
-
-  const submitTagItem = () => {
-    if (tagAmount < 5) {
-      let updatedTagList = [...tagList];
-      updatedTagList.push(tagItem);
-      setTagList(updatedTagList);
-      setTagItem("");
-      setTagAmout(tagAmount + 1);
-    } else {
-      window.alert("hi");
-    }
-  };
-
-  const deleteTagItem = (e) => {
-    const deleteTagItem = e.target.parentElement.firstChild.innerText;
-    const filteredTagList = tagList.filter(
-      (tagItem) => tagItem !== deleteTagItem
-    );
-    setTagList(filteredTagList);
-    setTagAmout(tagAmount - 1);
   };
 
   // const textOnChange = (e) => {
@@ -104,35 +72,7 @@ const Question = () => {
             ></textarea>
           </div>
         </form>
-        <div className="question-tag-container border-2 border-#E3E6E8 w-4/5 mb-12 rounded">
-          <div className="question-tag p-4 bg-#FFFFFF">
-            <div className="p-1">태그</div>
-            <div className="flex border-2 focus-within:border-pink-300 rounded">
-              <div className="flex">
-                {tagList.map((tagItem, index) => {
-                  return (
-                    <div
-                      className="p-1 tag-item inline-flex justify-center items-center rounded m-2 bg-sky-200"
-                      key={index}
-                    >
-                      <div className="ml-3 mr-1 bg-red">{tagItem}</div>
-                      <button className="ml-1 mr-3" onClick={deleteTagItem}>
-                        ❌
-                      </button>
-                    </div>
-                  );
-                })}
-              </div>
-              <input
-                className="flex-wrap w-full p-2 placeholder:italic bg-transparent outline-none"
-                type="text"
-                onChange={(e) => setTagItem(e.target.value)}
-                value={tagItem}
-                onKeyPress={onKeyPress}
-              ></input>
-            </div>
-          </div>
-        </div>
+        <Tag />
         <button
           className="question-post p-2 rounded border-none text-slate-50 bg-sky-400 w-40 h-12 text-sm"
           onClick={onPostClick}
--- client/src/components/question/Tag.jsx
+++ client/src/components/question/Tag.jsx
@@ -1,2 +1,72 @@
 // 재사용 가능하게 만들려면..?
 // input 으로 사용자가 지정한 태그를 끌어오고 return 목록
+import { useState } from "react";
+
+const Tag = () => {
+  const [tagItem, setTagItem] = useState("");
+  const [tagList, setTagList] = useState([]);
+  const [tagAmount, setTagAmout] = useState(0);
+
+  const onKeyPress = (e) => {
+    if (
+      e.target.value !== "" &&
+      e.key === "Enter" &&
+      tagList.indexOf(e.target.value) === -1
+    ) {
+      submitTagItem();
+    }
+  };
+
+  const submitTagItem = () => {
+    if (tagAmount < 5) {
+      let updatedTagList = [...tagList];
+      updatedTagList.push(tagItem);
+      setTagList(updatedTagList);
+      setTagItem("");
+      setTagAmout(tagAmount + 1);
+    } else {
+      window.alert("maximum 5 tags");
+    }
+  };
+
+  const deleteTagItem = (e) => {
+    const deleteTagItem = e.target.parentElement.firstChild.innerText;
+    const filteredTagList = tagList.filter(
+      (tagItem) => tagItem !== deleteTagItem
+    );
+    setTagList(filteredTagList);
+    setTagAmout(tagAmount - 1);
+  };
+  return (
+    <div className="question-tag-container border-2 border-#E3E6E8 w-4/5 mb-12 rounded">
+      <div className="question-tag p-4 bg-#FFFFFF">
+        <div className="p-1">태그</div>
+        <div className="flex flex-wrap w-full border-2 focus-within:border-pink-300 rounded">
+          {tagList.map((tagItem, index) => {
+            return (
+              <div
+                className="p-1 tag-item flex flex-row justify-center items-center rounded m-1 bg-sky-200"
+                key={index}
+              >
+                <div className="p-1">{tagItem}</div>
+                <button className="p-1" onClick={deleteTagItem}>
+                  ❌
+                </button>
+              </div>
+            );
+          })}
+
+          <input
+            className="w-auto p-2 bg-transparent outline-none"
+            type="text"
+            onChange={(e) => setTagItem(e.target.value)}
+            value={tagItem}
+            onKeyPress={onKeyPress}
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tag;
