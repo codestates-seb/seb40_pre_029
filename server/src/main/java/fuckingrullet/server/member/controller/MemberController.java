d4395e3ce6a5c645e994995ebad2310112d006c7
NewWorld_ <mf_cheonwol@naver.com>
2022-10-30 21:11:03 +0900

    feat: 회원 가입 기본 로직 구현

--- server/src/main/java/fuckingrullet/server/member/controller/MemberController.java
+++ server/src/main/java/fuckingrullet/server/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import fuckingrullet.server.domain.Member;
 import fuckingrullet.server.member.dto.MemberRegisterDto;
 import fuckingrullet.server.member.mapper.MemberMapper;
 import fuckingrullet.server.member.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +25,9 @@ public class MemberController {
 
     @PostMapping("/register")
     public ResponseEntity registerMember(@Valid @RequestBody MemberRegisterDto memberRegisterDto) {
-        memberService.registerMember(mapper.memberRegisterDtoToMember(memberRegisterDto));
-        return new ResponseEntity<>(null);
+        Member member = mapper.memberRegisterDtoToMember(memberRegisterDto);
+        Member registerMember = memberService.registerMember(member);
+        System.out.println(member);
+        return new ResponseEntity<>(registerMember, HttpStatus.CREATED);
     }
-
 }
