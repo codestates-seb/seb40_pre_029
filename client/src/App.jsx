84e0e838a4637cb8677ce2e8535dc1d7fc9fe3c6
daldalchoi <vocolate1990@gmail.com>
2022-10-31 16:16:39 +0900

    feat:Navigation 질문하기 css 및 기타 등등 css 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Main from "./pages/Main.jsx";
+import Ask from "./components/question/Ask.jsx";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col">
       <Main />
-    </>
+      <Ask />
+    </div>
   );
 }
 
--- client/src/components/question/Ask.jsx
+++ client/src/components/question/Ask.jsx
@@ -1,5 +1,4 @@
 //질문하기 버튼을 누르면 보이는 창이다.
-// /ask [POST] => { title , body , tags }
 import { useState } from "react";
 import Tag from "./Tag.jsx";
 
@@ -20,28 +19,30 @@ const Ask = () => {
           className="question-title-container border-2 border-#E3E6E8 w-4/5 mb-12 rounded"
           onSubmit={e => e.preventDefault()}>
           <div className="p-4 bg-#FFFFFF">
-            <div className="p-1">제목</div>
+            <div className="p-1">Title</div>
+            <div className="p-1 text-xs">Be specific and imagine you’re asking a question to another person.</div>
             <input
-              className="question-title rounded w-full p-2 placeholder:italic border-2 border-gray-400"
+              className="question-title rounded w-full p-2 border border-gray-400 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"
               onChange={e => setTitle(e.target.value)}
-              placeholder="title"
-              required></input>
+              required
+              placeholder="e.g Is there an R funtion for finding the index of an element in a vector?"></input>
           </div>
         </form>
         <form className="border-2 border-#E3E6E8 w-4/5 mb-12 rounded">
           <div className="p-4 bg-#FFFFFF ">
-            <div className="p-1">본문</div>
+            <div className="p-1">Body</div>
+            <div className="p-1 text-xs">
+              The body of your question contains your problem details and results. Minimum 30 characters.
+            </div>
             <textarea
               onChange={e => setBody(e.target.value)}
               rows={12}
-              className="question-body w-full p-2 bg-#F1F2F3 rounded border-2 border-gray-400 placeholder:italic resize-y "
-              placeholder="text area"
-              required></textarea>
+              className="question-body w-full p-2 bg-#F1F2F3 rounded border-2 border-gray-400 resize-y border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500 "></textarea>
           </div>
         </form>
         <Tag />
         <button
-          className="question-post p-2 rounded border-none text-slate-50 bg-sky-400 w-40 h-12 text-sm"
+          className="question-post p-2 rounded border-none text-slate-50 bg-sky-500 shadow-blue-500/50 w-36 h-12 text-sm"
           onClick={onPostClick}>
           Post your question
         </button>
