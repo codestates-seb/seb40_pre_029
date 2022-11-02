884c384e5ac0bfe2759422c0f6999690132718a2
NewWorld_ <mf_cheonwol@naver.com>
2022-11-02 09:50:42 +0900

    feat: 시큐리티 일부 로직 개선 및 프로젝트 명 변경 "UnStackOverFlow"

--- server/src/main/java/fuckingrullet/server/security/handler/MemberAuthenticationFailureHandler.java
+++ server/src/main/java/fuckingrullet/server/security/handler/MemberAuthenticationFailureHandler.java
@@ -36,7 +36,7 @@ public class MemberAuthenticationFailureHandler implements AuthenticationFailure
         if(exception instanceof UsernameNotFoundException){
             errorMessage = "존재하지 않는 계정입니다. 회원가입 후 로그인해주세요.";
         } else if (exception instanceof BadCredentialsException) {
-            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.";
+            errorMessage = "이메일 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.";
         } else if (exception instanceof InternalAuthenticationServiceException) {
             errorMessage = "내부 시스템 문제로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
         } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
@@ -45,7 +45,7 @@ public class MemberAuthenticationFailureHandler implements AuthenticationFailure
             errorMessage = "알 수 없는 오류로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
         }
 
-        log.info("failureHandler : " + errorMessage);
+        log.info("### failureHandler ### : " + errorMessage);
 
         Gson gson = new Gson();
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED, errorMessage);
