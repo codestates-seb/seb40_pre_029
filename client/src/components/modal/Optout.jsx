7b740e05889dee701c62e1b26aa9e8b87841902f
kwngmin <kwngmink@gmail.com>
2022-11-03 16:53:36 +0900

    fix:header, ask, question css 수정

--- client/src/components/modal/Optout.jsx
+++ client/src/components/modal/Optout.jsx
@@ -20,9 +20,9 @@ export default function OptoutModal() {
   //prettier-ignore
   return (
     <>
-      <div className="flex fixed inset-0 bg-gray-600 bg-opacity-80 h-full w-full z-50">
+      <div className="flex fixed inset-0 bg-gray-900 bg-opacity-80 h-full w-full z-50">
         <div className="flex-1 w-10 h-full relative justify-center items-center top-0 right-0">
-            <form className="w-30 z-0 absolute flex flex-col space-y-4 border-4 bg-white p-2 ">
+            <form className="w-[28rem] h-[40rem] z-0 absolute flex flex-col space-y-4 border-4 bg-white p-2 ">
               <span className="text-xl">비밀번호 확인</span>
               <input className="border p-2" name="password" placeholder="Password" onChange={onChange} value={inputs.password} type='password'></input>
               {isValid? null:<span className={inputs.length === 0?"text-black-500":"text-red-500"}>비밀번호에는 영문,숫자,특수문자만 입력 가능합니다.</span>}
