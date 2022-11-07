5bd12459babe89a0a8354dd49125091188561eb4
DalDalChoi <107661312+DalDalChoi@users.noreply.github.com>
2022-11-07 10:38:30 +0900

    Update Details.jsx

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -33,7 +33,7 @@ export default function Details() {
 
   if (data.length === 0) return <Spinner />;
   return (
-    <div className="w-full pt-10 pb-4 mr-8 h-screen">
+    <div className="w-full pt-10 pb-4 mr-8 xl:h-[60rem]">
       <div className="pl-12 pb-6 border-b border-gray-300">
         <h3 className="text-3xl mb-4 font-medium">{data.title}</h3>
         <div>
