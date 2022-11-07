ea27052e060fec11a6f2813e57617e5bb03496a7
lactofreemilk <jihwan4743@gmail.com>
2022-11-07 15:04:50 +0900

    stage complete

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -1,15 +1,21 @@
 import OptoutModal from "../components/modal/Optout.jsx";
 import { useState, useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const MyProfile = state => {
+  const navigate = useNavigate();
   const [optoutcheck, setOptoutCheck] = useState(false);
   const [editcheck, setEditCheck] = useState(false);
   const [pw, isPW] = useState({
+    nickname: "",
     pw: "",
     pwc: "",
     pwsame: false,
   });
   const pwpattern = new RegExp("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$");
+  const nicknamepattern = new RegExp("^[a-zA-Z0-9가-힣]+$");
+
   const userMenu = useRef(null);
   const modalCloseHandler = ({ target }) => {
     if (typeof userMenu.current === "undefined" || userMenu.current === null) {
@@ -19,17 +25,15 @@ const MyProfile = state => {
       console.log(optoutcheck);
     }
   };
+  const token = useSelector(state => state.authorization);
 
   const onChange = async ele => {
     const { value, name } = ele.target;
     //prettier-ignore
-
-    if (pwpattern.test(value)) {
-      isPW({
-        ...pw,
-        [name]: value,
-      });
-    }
+    isPW({
+      ...pw,
+      [name]: value,
+    });
   };
 
   useEffect(() => {
@@ -50,36 +54,41 @@ const MyProfile = state => {
     if (id === "optout") setOptoutCheck(true);
     if (id === "edit") setEditCheck(!editcheck);
     if (id === "cancel") setEditCheck(false);
-    if (id === "editdone") editProcess;
   }
 
   const editProcess = async data => {
     data.preventDefault();
     let info = {};
-    let nickname = data.target[0].value;
-    let pw = data.target[1].vaule;
-    let pwc = data.target[1].vaule;
-    if (nickname) info.nickname = nickname;
-    if (pw && pw === pwc) info.password = pw;
+    if (pw.pw !== "" && pw.pw !== pw.pwc) {
+      alert("비밀번호가 같지 않습니다");
+      return;
+    }
+    if (pw.pw === "" && pw.pwc === "" && pw.nickname === "") {
+      alert("입력된 정보가 없습니다");
+      return;
+    } else {
+      if (pw.nickname !== "" && nicknamepattern.test(pw.nickname)) info.displayName = pw.nickname;
+      else return alert("닉네임이 형식에 맞지 않습니다(한글,영문,숫자 가능)");
+      if (pw.pw !== "") {
+        if (pw.pw !== "" && pwpattern.test(pw.pw)) info.password = pw.pw;
+        else return alert("비밀번호가 형식에 맞지 않습니다(영문,숫자,특수문자 포함 8-16자)");
+      }
+    }
 
     //prettier-ignore
     const response = await fetch("/api/auth/member", {
       method: "PATCH",
-      headers: { "Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json", authorization: token},
       body: JSON.stringify(info),
       })
     let res = response;
     if (!res.ok) {
-      await res.json().then(data => alert(data));
+      await res.json().then(alert("문제가 발생하였습니다."));
     } else {
-      await res.json().then(data => console.log(data));
-      alert("로그인이 완료되었습니다");
-      console.log(res.headers.get("refresh"));
-      console.log(response.headers.get("authorization"));
-      console.log("이렇게하면" + res.headers.refresh);
-
-      // setLogin(true);
-      // return location.reload();
+      await res.json().then(data => {
+        setEditCheck(!editcheck);
+        navigate("/myprofile", { state: data });
+      });
     }
   };
 
@@ -105,7 +114,11 @@ const MyProfile = state => {
             <h1 className="items-center flex text-2xl mt-1 font-medium">Nickname</h1>
             {/* 여기에 입력받는 닉네임 추가 */}
             {editcheck ? (
-              <input className="text-sm rounded w-96 p-2 border border-gray-400 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"></input>
+              <input
+                name="nickname"
+                onChange={onChange}
+                value={pw.nickname}
+                className="text-sm rounded w-96 p-2 border border-gray-400 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"></input>
             ) : (
               <h1 className="items-center flex text-2xl mt-1">{state.state.displayName}</h1>
             )}
@@ -115,24 +128,23 @@ const MyProfile = state => {
               <div className="flex justify-between h-16 pl-14 mb-4">
                 <h1 className="items-center flex text-2xl mt-1 font-medium">Password Change</h1>
                 <input
+                  type="password"
                   name="pw"
                   onChange={onChange}
+                  value={pw.pw}
                   className="text-sm rounded w-96 p-2 border border-gray-400 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"></input>
               </div>
               <div className="flex justify-between h-16 pl-14 mb-4">
                 <h1 className="items-center flex text-2xl mt-1 font-medium">Confirm Password</h1>
                 <input
+                  type="password"
                   name="pwc"
+                  value={pw.pwc}
                   onChange={onChange}
                   className="text-sm rounded w-96 p-2 border border-gray-400 border rounded border-gray-300 focus:text-black focus:outline-none focus:border-emerald-500 focus:ring-4 focus:border focus:ring-emerald-100 text-gray-500"></input>
               </div>
             </div>
           ) : null}
-          {pw.pwsame ? (
-            <span className="h-5 w-96 mb-1"></span>
-          ) : (
-            <span className="text-black-500 text-sm h-5 w-96 mb-1">위의 비밀번호와 동일하여야 합니다.</span>
-          )}
           <div className="flex flex-row-reverse h-20">
             {editcheck ? (
               <button
@@ -154,7 +166,7 @@ const MyProfile = state => {
               <button
                 className="p-2 mb-4 mx-2 rounded border-none text-slate-50 bg-sky-500 shadow-blue-500/50 shadow w-36 h-12 text-sm"
                 id="editdone"
-                onClick={openModalHandler}>
+                onClick={editProcess}>
                 Edit
               </button>
             ) : (
